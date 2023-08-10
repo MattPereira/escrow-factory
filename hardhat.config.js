5fa2751c504cbcb83fb43f1923864f7ab7bb8eda
Matthew Pereira <matthewdavidpereira@gmail.com>
2023-08-10 15:03:53 -0700

    finish project

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,18 @@
-require('@nomicfoundation/hardhat-toolbox');
+require("dotenv").config();
+require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
   solidity: "0.8.17",
   paths: {
     artifacts: "./app/src/artifacts",
-  }
+  },
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
